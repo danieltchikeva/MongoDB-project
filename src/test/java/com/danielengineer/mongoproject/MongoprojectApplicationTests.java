a87511a4edb33e1a3a59e0e38b00fa84aebd5968
Daniel Tchikeva <109494655+danieltchikeva@users.noreply.github.com>
2023-11-26 10:42:53 -0300

    Project Created

---
+++ src/test/java/com/danielengineer/mongoproject/MongoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielengineer.mongoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
