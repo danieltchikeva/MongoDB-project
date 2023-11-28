b671e4b490e738c3132ffd8101880fb19dd0ed08
Daniel Tchikeva <109494655+danieltchikeva@users.noreply.github.com>
2023-11-28 10:24:56 -0300

    Consulta com vários critérios

--- src/main/java/com/danielengineer/mongoproject/services/PostService.java
+++ src/main/java/com/danielengineer/mongoproject/services/PostService.java
@@ -1,5 +1,6 @@
 package com.danielengineer.mongoproject.services;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,5 +33,9 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 	
 }
